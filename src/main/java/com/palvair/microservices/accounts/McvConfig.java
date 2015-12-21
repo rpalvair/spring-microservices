3077cd682812a634b09d738300c7f42809c363dc
palvairr <rudy.palvair@xpologistics.com>
2015-12-21 17:44:09 +0100

    Refactoring

--- src/main/java/com/palvair/microservices/accounts/McvConfig.java
+++ src/main/java/com/palvair/microservices/accounts/McvConfig.java
@@ -3,13 +3,18 @@ package com.palvair.microservices.accounts;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  * Created by PALVAIRR on 21/12/2015.
  */
 @Configuration
+@EnableWebMvc
 public class McvConfig  extends WebMvcConfigurerAdapter {
 
     @Bean
@@ -18,5 +23,10 @@ public class McvConfig  extends WebMvcConfigurerAdapter {
         ObjectMapper objectMapper = new ObjectMapper();
         jsonConverter.setObjectMapper(objectMapper);
         return jsonConverter;
+    }
+
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(jsonConverter());
+        super.configureMessageConverters(converters);
     }
 }
